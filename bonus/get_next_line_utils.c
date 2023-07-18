2f2d2fb54906cf3eb24a2bba019462cabd1bc54f
Gilles Gneme <gilles@DESKTOP-QF0T62A>
2023-07-18 16:16:03 +0100

    New changes

--- bonus/get_next_line_utils.c
+++ bonus/get_next_line_utils.c
@@ -12,7 +12,7 @@
 
 #include "../push_swap.h"
 
-static size_t	ft_strlcpy_mod(char *dest, const char *src, size_t size)
+static void	ft_strlcpy_mod(char *dest, const char *src, size_t size)
 {
 	size_t	n;
 
@@ -26,7 +26,6 @@ static size_t	ft_strlcpy_mod(char *dest, const char *src, size_t size)
 		}
 		dest[n] = '\0';
 	}
-	return (0);
 }
 
 size_t	ft_strlen(const char *str)
@@ -39,23 +38,20 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
-static size_t	ft_strlcat_mod(char *dest, const char *src, size_t size)
+static void	ft_strlcat_mod(char *dest, const char *src, size_t size)
 {
 	size_t	n;
 	size_t	i;
-	size_t	len;
 
 	if (size == 0 && !dest)
-		return (ft_strlen(src));
+		return ;
 	i = ft_strlen(dest);
-	len = i;
 	n = 0;
 	if (size <= i)
-		return (size + ft_strlen(src));
+		return ;
 	while (src[n] != '\0' && i < size - 1)
 		dest[i++] = src[n++];
 	dest[i] = '\0';
-	return (0);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
